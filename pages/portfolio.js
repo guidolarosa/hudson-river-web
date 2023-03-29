eb1c0fc063dca668e06299c212a82909e8b19bc2
guidolarosa <guido.glarosa@gmail.com>
2023-03-29 08:24:22 -0300

    Added portfolio itemsand styles

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -3,10 +3,14 @@ import Root from '@/components/Root';
 import styled from 'styled-components';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
+import { useState } from 'react';
+import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
 
 const inter = Inter({subsets: ['latin'], weight: ['300', '400', '500', '600', '700']});
 
 export default function Portfolio(props) {
+  const [currentView, setCurrentView] = useState('rea');
   return (
     <>
       <Head>
@@ -18,38 +22,78 @@ export default function Portfolio(props) {
       <StyledRoot>
         <div className="sidebar">
           <ul>
-            <li>
+            <li 
+              role="button" 
+              onClick={() => { setCurrentView('rea') }}
+              className={`${currentView === 'rea' ? 'current' : ''}`}
+            >
               Real Estate Acquisitions
             </li>
-            <li>
+            <li 
+              role="button" 
+              onClick={() => { setCurrentView('investments') }}
+              className={`${currentView === 'investments' ? 'current' : ''}`}
+            >
               Investments
             </li>
           </ul>
         </div>
         <main className="list-content">
-          <div className="investments">
-            <ul>
-              {props.rsa.data.map((content, index) => (
-                <li key={index}>
-                  <div className="portfolio-item-card">
-                    <div className="header">
-                      <div className="image">
-                        <Image
-                          fill
-                          src={content.attributes.MainImage.data.attributes.url}
-                          alt={content.attributes.Name}
-                        />
+          {currentView === 'rea' ? (
+            <div className="rea">
+              <ul>
+                {props.rea.data.map((content, index) => (
+                  <li key={index}>
+                    <Link href={`/real-estate-aquicisitions/${content.attributes.Slug}`}>
+                      <div className="portfolio-item-card">
+                        <div className="header">
+                          <div className="image">
+                            <Image
+                              fill
+                              src={content.attributes.MainImage.data.attributes.url}
+                              alt={content.attributes.Name}
+                            />
+                          </div>
+                        </div>
+                        <div className="body">
+                          <div className={`city ${inter.className}`}>{content.attributes.City}</div>
+                          <h2>{content.attributes.Name}</h2>
+                        </div>
                       </div>
-                    </div>
-                    <div className="body">
-                      <div className={`city ${inter.className}`}>{content.attributes.City}</div>
-                      <h2>{content.attributes.Name}</h2>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <div className="investments">
+              <ul>
+                {props.investments.data.map((content, index) => (
+                  <li key={index}>
+                    <Link href={`/investments/${content.attributes.Slug}`}>
+                      <div className="portfolio-item-card">
+                        <div className="header">
+                          <div className="image">
+                            <Image
+                              fill
+                              src={content.attributes.MainImage.data.attributes.url}
+                              alt={content.attributes.Name}
+                            />
+                          </div>
+                        </div>
+                        <div className="body">
+                          <h2>{content.attributes.Name}</h2>
+                          <ReactMarkdown className={inter.className}>
+                            {content.attributes.Excerpt}
+                          </ReactMarkdown>
+                        </div>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </main>
       </StyledRoot>
     </>
@@ -67,13 +111,42 @@ const StyledRoot = styled(Root)`
     grid-template-rows: 100%;
     grid-template-areas: 
       'sidebar lists';
+    grid-column-gap: 4rem;
   }
   .sidebar {
     grid-area: sidebar;
     ul {
       li {
-        font-size: 3rem;
+        font-size: 4rem;
         margin-bottom: 4rem;
+        transition: 0.25s ease-in-out color;
+        cursor: pointer;
+        border-bottom: 1px solid ${props => props.theme.colors.gold100};
+        padding-bottom: 2rem;
+        position: relative;
+        &:before {
+          content: '';
+          width: 2rem;
+          height: 2rem;
+          background-image: url('/general/arrow.svg');
+          background-size: contain;
+          background-position: center;
+          background-repeat: no-repeat;
+          position: absolute;
+          left: -4rem;
+          top: 1.5rem;
+          opacity: 0;
+          transition: 0.25s ease-in-out opacity;
+        }
+        &:last-of-type {
+          border-bottom: none;
+        }
+        &.current {
+          color: ${props => props.theme.colors.gold500};
+          &:before {
+            opacity: 1;
+          }
+        }
       }
     }
   }
@@ -97,8 +170,11 @@ const StyledRoot = styled(Root)`
             margin-bottom: 3rem;
             .image {
               position: relative;
-              height: 35.25rem;
+              height: 25rem;
               width: 100%;
+              @media ${props => props.theme.bp.md} {
+                height: 35.25rem;
+              }
               img {
                 object-fit: cover;
               }
@@ -115,7 +191,41 @@ const StyledRoot = styled(Root)`
               font-size: 5rem;
               font-weight: 700;
               line-height: 6rem;
+              margin-bottom: 1rem;
             }
+          }
+        }
+      }
+    }
+    .investments {
+      .portfolio-item-card {
+        .header {
+          width: 100%;
+          height: 25rem;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background: ${props => props.theme.colors.gold25};
+          @media ${props => props.theme.bp.md} {
+            height: 35.25rem;
+          }
+          .image {
+            max-width: 25rem;
+            max-height: 10.5rem;
+            img {
+              object-fit: contain;
+              object-position: center;
+            }
+          }
+        }
+        .body {
+          font-weight: 300;
+          p {
+            font-size: 2rem;
+            margin-bottom: 1rem;
+          }
+          a {
+            color:  ${props => props.theme.colors.gold500};
           }
         }
       }
@@ -127,13 +237,13 @@ export async function getStaticProps(context) {
   const investmentsRes = await fetch('https://hudson-river-admin.herokuapp.com/api/investments?populate=*');
   const investments = await investmentsRes.json();
 
-  const rsaRes = await fetch('https://hudson-river-admin.herokuapp.com/api/real-estate-acquisitions?populate=*');
-  const rsa = await rsaRes.json();
+  const reaRes = await fetch('https://hudson-river-admin.herokuapp.com/api/real-estate-acquisitions?populate=*');
+  const rea = await reaRes.json();
 
   return {
     props: {
       investments: investments,
-      rsa: rsa
+      rea: rea
     }
   }
 }
