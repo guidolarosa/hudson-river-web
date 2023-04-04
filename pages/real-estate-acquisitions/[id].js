62330a00807d273a96de4ec962643c28202f95ea
guidolarosa <guido.glarosa@gmail.com>
2023-04-04 16:29:11 -0300

    Updated styles for portfolio item page

--- pages/real-estate-acquisitions/[id].js
+++ pages/real-estate-acquisitions/[id].js
@@ -147,57 +147,60 @@ export default function REA(props) {
               </div>
             )}
           </div>
-          <div className="slider slider-desktop">
-            <Splide
-              onActive={
-                (e) => {
-                  setActiveImage(e.index)
+          {SliderImages.data && (
+
+            <div className="slider slider-desktop">
+              <Splide
+                onActive={
+                  (e) => {
+                    setActiveImage(e.index)
+                  }
                 }
-              }
-              options={{
-                type: 'loop',
-                direction: 'ttb',
-                height: '43rem',
-                perPage: 3,
-                perMove: 1,
-                pagination: false
-              }}
-              hasTrack={false}
-            >
-              <SplideTrack>
-                {SliderImages.data.map((image, index) => (
-                  <SplideSlide key={index}>
-                    <div className="slide-image">
-                      <Image
-                        fill
-                        alt={image.attributes.name}
-                        src={image.attributes.url}
-                      />
-                    </div>
-                    <div className="index">
-                      <span>{index >= 10 ? index + 1 : '0' + (index + 1)}</span>
-                    </div>
-                  </SplideSlide>
-                ))}
-              </SplideTrack>
-              <div className="splide__arrows">
-                <button className="splide__arrow splide__arrow--prev">
-                  <Image
-                    fill
-                    src="/general/caret-left.svg"
-                    alt={'Previous'}
-                  />
-                </button>
-                <button className="splide__arrow splide__arrow--next">
-                  <Image
-                    fill
-                    src="/general/caret-right.svg"
-                    alt={'Next'}
-                  />
-                </button>
-              </div>
-            </Splide>
-          </div>
+                options={{
+                  type: 'loop',
+                  direction: 'ttb',
+                  height: '43rem',
+                  perPage: 3,
+                  perMove: 1,
+                  pagination: false
+                }}
+                hasTrack={false}
+              >
+                <SplideTrack>
+                  {SliderImages.data.map((image, index) => (
+                    <SplideSlide key={index}>
+                      <div className="slide-image">
+                        <Image
+                          fill
+                          alt={image.attributes.name}
+                          src={image.attributes.url}
+                        />
+                      </div>
+                      <div className="index">
+                        <span>{index >= 10 ? index + 1 : '0' + (index + 1)}</span>
+                      </div>
+                    </SplideSlide>
+                  ))}
+                </SplideTrack>
+                <div className="splide__arrows">
+                  <button className="splide__arrow splide__arrow--prev">
+                    <Image
+                      fill
+                      src="/general/caret-left.svg"
+                      alt={'Previous'}
+                    />
+                  </button>
+                  <button className="splide__arrow splide__arrow--next">
+                    <Image
+                      fill
+                      src="/general/caret-right.svg"
+                      alt={'Next'}
+                    />
+                  </button>
+                </div>
+              </Splide>
+            </div>
+          )}
           <div className={`modal ${isModalOpen ? 'open' : ''}`}>
             <div 
               className="close-modal" 
