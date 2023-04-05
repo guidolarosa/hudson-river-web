371a5a9230a3383b53a9b768285fdb8a32170f5e
guidolarosa <guido.glarosa@gmail.com>
2023-04-05 13:58:17 -0300

    Updated init animation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Root from '@/components/Root';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Image from 'next/image';
 import { fadeUp } from '@/theme/animations';
 
@@ -14,6 +14,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <StyledRoot>
+        <div className="init-animation">
+          <div className="init-image">
+            <object 
+              type="image/svg+xml" 
+              data="/homepage/intro.svg"
+            />
+          </div>
+        </div>
         <div className="hero">
           <div className="hero-content">
             <h1>Real Estate Private <br/> Equity & Investments</h1>
@@ -36,7 +44,26 @@ export default function Home() {
   )
 };
 
+const fadeOut = keyframes`
+  0% {opacity: 1};
+  100% {opacity: 0};
+`;
+
 const StyledRoot = styled(Root)`
+  .init-animation {
+    background: ${props => props.theme.colors.offWhite};
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    pointer-events: none;
+    animation: 1s ${fadeOut} 2s forwards ease-out;
+  }
   .hero {
     height: 100%;
     flex-grow: 1;
@@ -62,7 +89,7 @@ const StyledRoot = styled(Root)`
       position: relative;
       z-index: 1;
       opacity: 0;
-      animation: 0.25s ${fadeUp} 1s ease-out forwards;
+      animation: 0.25s ${fadeUp} 2.25s ease-out forwards;
       @media ${props => props.theme.bp.md} {
         margin-top: 11rem;
       }
